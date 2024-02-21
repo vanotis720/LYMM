2b9040ceddec6a49df7368af37184c7074eb67c6
vander makadi <vanotis720@gmail.com>
2024-02-21 10:58:35 +0200

    changed img url on 'nos offres' section

--- src/components/Offres.jsx
+++ src/components/Offres.jsx
@@ -1,12 +1,8 @@
-import banner from "../assets/image/002-conseils-eclairage-piece-par-piece.jpg";
-
 const Offres = () => {
-  console.log(banner);
-
   return (
     <div>
       <div
-        className={`bg-[url(${banner})] w-full h-[40vh] sm:h-[90vh] bg-cover bg-center`}
+        className={`bg-[url("./src/assets/image/002-conseils-eclairage-piece-par-piece.jpg")] w-full h-[40vh] sm:h-[90vh] bg-cover bg-center`}
       >
         <div className="bg-black w-[90%] sm:h-[70vh] h-[30vh] m-auto opacity-75 relative sm:bottom-20 bottom-8 rounded-t-3xl ">
           <h2 className="text-white flex justify-self-center">Nos offres</h2>
